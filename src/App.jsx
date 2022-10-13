80899d0b33b04bb95ebe77e560a8b29de79a5262
gunhee-jeong <kyle4619@naver.com>
2022-10-13 18:14:39 +0900

    Add: HeaderPage 의 기본적인 랜더링을 위한 액션을 테스트

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,12 @@ import HomePage from './HomePage';
 import LoginPage from './LoginPage/LoginPage';
 import NotFoundPage from './NotFoundPage/NotFoundPage';
 import FooterPage from './Footer/FooterPage';
+import HeaderPage from './Header/HeaderPage';
 
 export default function App() {
   return (
     <>
-      <div>Header</div>
+      <HeaderPage />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
---
+++ src/Header/HeaderPage.jsx
@@ -0,0 +1,5 @@
+export default function HeaderPage() {
+  return (
+    <div>Header!</div>
+  );
+}
