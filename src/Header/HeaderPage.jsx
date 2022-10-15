f282837968baed6de28fa3824368b99f9a31cdc9
gunhee-jeong <kyle4619@naver.com>
2022-10-15 18:09:36 +0900

    Add: PurchaseContainer 의 icons 를 렌더링하는 액션을 구현 및 테스트

--- src/Header/HeaderPage.jsx
+++ src/Header/HeaderPage.jsx
@@ -1,30 +1,6 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faCartShopping,
-  faCircleUser,
-} from '@fortawesome/free-solid-svg-icons';
-
 import TitleContainer from './TitleContainer';
 import SearchBarContainer from './SearchBarContainer';
-
-function PurchaseContainer() {
-  return (
-    <>
-      <FontAwesomeIcon
-        title="shoppingCart"
-        icon={faCartShopping}
-        size="2x"
-        color="red"
-      />
-      <FontAwesomeIcon
-        title="circleUser"
-        icon={faCircleUser}
-        size="2x"
-        color="red"
-      />
-    </>
-  );
-}
+import PurchaseContainer from './PurchaseContainer';
 
 export default function HeaderPage() {
   return (
