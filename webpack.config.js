5a8d549f83b1aaec244d4816ba13afeb9f74036e
gunhee-jeong <kyle4619@naver.com>
2022-10-28 19:36:11 +0900

    Add: 서버 통신을 위한 fireBase의 설정을 추가 - .env를 사용하여 API_KEY를 노출되지 않도록 설정 - webpackconfig 파일에 plugins를 사용하여 process.env로 환경변수를 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,37 @@
 const path = require('path');
+const webpack = require('webpack');
 
-module.exports = {
-  entry: path.resolve(__dirname, 'src/index.jsx'),
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  devServer: {
-    historyApiFallback: {
-      index: 'index.html',
+const dotenv = require('dotenv');
+
+module.exports = () => {
+  dotenv.config();
+
+  return {
+    entry: path.resolve(__dirname, 'src/index.jsx'),
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+      ],
     },
-  },
-  // devServer: {
-  //   historyApiFallback: true,
-  // },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    devServer: {
+      historyApiFallback: {
+        index: 'index.html',
+      },
+    },
+    // devServer: {
+    //   historyApiFallback: true,
+    // },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': JSON.stringify(process.env),
+      }),
+    ],
+  };
 };
