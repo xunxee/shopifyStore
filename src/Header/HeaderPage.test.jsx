fd984628542173aaaaa58e2584e09b2d2d60bcc6
gunhee-jeong <kyle4619@naver.com>
2022-11-09 02:07:11 +0900

    Refactor: HeaderPage 컴포넌트의 액션을 리팩터링 - isModalOpen의 value에 따라 분기되는 테스트를 추가

--- src/Header/HeaderPage.test.jsx
+++ src/Header/HeaderPage.test.jsx
@@ -1,8 +1,16 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setIsModalOpen } from '../LoginPage/slice';
 
 import HeaderPage from './HeaderPage';
 
+jest.mock('react-redux');
+
 describe('HeaderPage', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
   it('renders the categories list', () => {
     const { container } = render((
       <HeaderPage />
@@ -12,14 +20,57 @@ describe('HeaderPage', () => {
   });
 
   it('renders the search bar', () => {
-    const { queryByPlaceholderText } = render(<HeaderPage />);
+    const { queryByPlaceholderText } = render((
+      <HeaderPage />
+    ));
 
-    expect(queryByPlaceholderText('Search for products...')).not.toBeNull();
+    expect(queryByPlaceholderText('Search for products...'))
+      .not.toBeNull();
   });
 
   it('renders the shopping cart', () => {
-    const { queryByTitle } = render(<HeaderPage />);
+    const { queryByTitle } = render((
+      <HeaderPage />
+    ));
 
     expect(queryByTitle('shoppingCart')).not.toBeNull();
+  });
+
+  it('renders "circleUser" icon', () => {
+    const { getByRole } = render((
+      <HeaderPage />
+    ));
+
+    fireEvent.click(getByRole(
+      'button',
+      { name: 'circleUser' },
+    ));
+
+    expect(dispatch).toBeCalledWith(setIsModalOpen());
+  });
+
+  context('when isModalOpen is true', () => {
+    beforeEach(() => {
+      useSelector.mockImplementation((selector) => selector({
+        login: {
+          isModalOpen: true,
+          loginFields: {
+            email: '',
+            password: '',
+            firstName: '',
+            lastName: '',
+            error: '',
+          },
+        },
+      }));
+    });
+
+    it('renders LoginPage', () => {
+      const { queryByPlaceholderText } = render((
+        <HeaderPage />
+      ));
+
+      expect(queryByPlaceholderText('Email')).not.toBeNull();
+    });
   });
 });
