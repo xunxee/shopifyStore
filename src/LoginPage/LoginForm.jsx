80fb0e7d464c2a13d1c677a599595c1ef9dffe34
gunhee-jeong <kyle4619@naver.com>
2022-11-12 12:52:30 +0900

    Add: Vercel 사용을 위한 package.json 설정 추가

--- src/LoginPage/LoginForm.jsx
+++ src/LoginPage/LoginForm.jsx
@@ -1,4 +1,11 @@
+import styled from '@emotion/styled';
+
 import { memo } from 'react';
+
+const Container = styled.form({
+  display: 'flex',
+  flexDirection: 'column',
+});
 
 export default memo(({
   isLogin,
@@ -13,51 +20,53 @@ export default memo(({
   }
 
   return (
-    <form onSubmit={onSubmit}>
+    <>
       {error && <p>{error}</p>}
-      {isLogin || (
-        <>
-          <input
-            name="lastName"
-            type="text"
-            placeholder="성(Last Name)"
-            value={lastName}
-            onChange={handleChange}
-          />
-          <input
-            name="firstName"
-            type="text"
-            placeholder="이름(First Name)"
-            value={firstName}
-            onChange={handleChange}
-          />
-        </>
-      )}
-      <input
-        name="email"
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={handleChange}
-      />
-      <input
-        name="password"
-        type="password"
-        placeholder="Password"
-        value={password}
-        autoComplete="off"
-        onChange={handleChange}
-      />
-      <button
-        type="submit"
-      >
-        {isLogin ? 'Log In' : 'Sign Up'}
-      </button>
-      <p>
-        {isLogin
-          ? "Don't have an account?"
-          : 'Passwords must be longer than 7 chars and include numbers'}
-      </p>
-    </form>
+      <Container onSubmit={onSubmit}>
+        {isLogin || (
+          <>
+            <input
+              name="lastName"
+              type="text"
+              placeholder="성(Last Name)"
+              value={lastName}
+              onChange={handleChange}
+            />
+            <input
+              name="firstName"
+              type="text"
+              placeholder="이름(First Name)"
+              value={firstName}
+              onChange={handleChange}
+            />
+          </>
+        )}
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={handleChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          value={password}
+          autoComplete="off"
+          onChange={handleChange}
+        />
+        <button
+          type="submit"
+        >
+          {isLogin ? 'Log In' : 'Sign Up'}
+        </button>
+        <p>
+          {isLogin
+            ? "Don't have an account?"
+            : 'Passwords must be longer than 7 chars and include numbers'}
+        </p>
+      </Container>
+    </>
   );
 });
