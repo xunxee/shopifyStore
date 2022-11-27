a60e3a6c52f28ecde973ca4582bd1d954cd42434
gunhee-jeong <kyle4619@naver.com>
2022-11-27 21:18:41 +0900

    Modify: Footer 컴포넌트의 width를 반응형으로 수정 - width가 1920px 이후부터는 늘어나지 않도록 수정

--- src/Footer/FooterTopContainer.jsx
+++ src/Footer/FooterTopContainer.jsx
@@ -7,6 +7,10 @@ import FooterTopInfo from './FooterTopInfo';
 const Container = styled.div({
   display: 'grid',
   gridTemplateColumns: 'repeat(12, 1fr)',
+  width: '90%',
+  maxWidth: '1920px',
+  marginRight: 'auto',
+  marginLeft: 'auto',
   paddingTop: '3rem',
   paddingBottom: '3rem',
 });
