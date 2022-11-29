ff4510911d986ea974c12e3fcc738dedbbbaf67e
gunhee-jeong <kyle4619@naver.com>
2022-11-29 21:45:53 +0900

    Refactor: ListPage 컴포넌트의 불필요한 CSS 코드를 리팩터링 - 기존의 불필요한 “flex:1” 코드를 수정 - ListPage 컴포넌트의 backgroundColor를 추가

--- src/List/ListPage.jsx
+++ src/List/ListPage.jsx
@@ -3,9 +3,9 @@ import styled from '@emotion/styled';
 import ListContainer from './ListContainer';
 
 const Container = styled.div({
-  flex: '1',
   justifyContent: 'space-around',
   minWidth: '950px',
+  backgroundColor: '#000',
 });
 
 export default function ListPage() {
