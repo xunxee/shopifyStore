a0a0a740df589ec3c36f627699bf5f7249f735e7
gunhee-jeong <kyle4619@naver.com>
2022-12-04 20:39:46 +0900

    Fix: FooterTopList 컴포넌트에 unique key를 주지 않아 발생한 에러를 해결 - Item에 key 프로퍼티를 설정하여 에러를 해결

--- src/Footer/FooterTopList.jsx
+++ src/Footer/FooterTopList.jsx
@@ -24,30 +24,35 @@ export default function FooterTopList() {
     <Container>
       {[
         {
+          id: 1,
           column: 1,
           rowStart: 1,
           rowEnd: 2,
           title: 'Home',
         },
         {
+          id: 2,
           column: 1,
           rowStart: 2,
           rowEnd: 3,
           title: 'About',
         },
         {
+          id: 3,
           column: 1,
           rowStart: 3,
           rowEnd: 4,
           title: 'Terms of use',
         },
         {
+          id: 4,
           column: 1,
           rowStart: 4,
           rowEnd: 5,
           title: 'Shopping',
         },
         {
+          id: 5,
           column: 2,
           rowStart: 1,
           rowEnd: 2,
@@ -55,6 +60,7 @@ export default function FooterTopList() {
         },
       ].map((item) => (
         <Item
+          key={item.id}
           column={item.column}
           rowStart={item.rowStart}
           rowEnd={item.rowEnd}
