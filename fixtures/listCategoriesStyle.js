081e4c64da7748933ae4d83ed03098bb30e1ee35
gunhee-jeong <kyle4619@naver.com>
2022-12-04 22:49:09 +0900

    Add: fixtures 디렉터리에 listCategoriesStyle.js를 추가 - CategoryBar 컴포넌트에서 반복 사용될 CSS 스타일을 추가

---
+++ fixtures/listCategoriesStyle.js
@@ -0,0 +1,15 @@
+const listCategoriesStyle = {
+  layout: {
+    marginTop: '2rem',
+    fontSize: '20px',
+    fontWeight: '700',
+  },
+  items: {
+    marginTop: '1.5rem',
+    fontSize: '17px',
+    fontWeight: '500',
+    cursor: 'pointer',
+  },
+};
+
+export default listCategoriesStyle;
