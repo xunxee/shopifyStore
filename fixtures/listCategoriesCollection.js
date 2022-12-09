e7a8974d9b48e323b48631e90d2f940fda452488
gunhee-jeong <kyle4619@naver.com>
2022-12-09 20:53:58 +0900

    Add: fixtures/listCategoriesCollection.js를 생성 - List 페이지에서 사용되는 categories들의 목록을 모아두는 fixtures를 생성하여 관리 - CategoryBar 컴포넌트에서 해당 fixtures를 연결하여 사용

---
+++ fixtures/listCategoriesCollection.js
@@ -0,0 +1,43 @@
+const listCategoriesCollection = {
+  categories:
+    [
+      { id: 1, title: 'New Arrivals', name: 'new' },
+      { id: 2, title: 'Featured', name: 'featured' },
+    ],
+  products: [
+    { id: 1, title: 'Beds', name: 'beds' },
+    { id: 2, title: 'Sofas', name: 'sofas' },
+    { id: 3, title: 'Tables & desks', name: 'tables' },
+    { id: 4, title: 'Chairs', name: 'chairs' },
+  ],
+  sort: [
+    {
+      id: 1,
+      title: 'Trending',
+      name: 'trending',
+    },
+    {
+      id: 2,
+      title: 'Latest arrivals',
+      name: 'latestArrivals',
+    },
+    {
+      id: 3,
+      title: 'Price: Low to high',
+      name: 'lowToHigh',
+    },
+    {
+      id: 4,
+      title: 'Price: High to low',
+      name: 'highToLow',
+    },
+  ],
+  material: [
+    { id: 1, title: 'Fabric', name: 'fabric' },
+    { id: 2, title: 'Wood', name: 'wood' },
+    { id: 3, title: 'Metal', name: 'metal' },
+    { id: 4, title: 'Glass', name: 'glass' },
+  ],
+};
+
+export default listCategoriesCollection;
--- src/List/CategoryBar.jsx
+++ src/List/CategoryBar.jsx
@@ -3,6 +3,7 @@ import { useCallback } from 'react';
 import styled from '@emotion/styled';
 
 import LIST_CATEGORIES_STYLE from '../../fixtures/listCategoriesStyle';
+import LIST_CATEGORIES from '../../fixtures/listCategoriesCollection';
 
 const {
   container,
@@ -42,10 +43,7 @@ export default function CategoryBar({
       <div>
         <Layout>
           All Categories
-          {[
-            { id: 1, title: 'New Arrivals', name: 'new' },
-            { id: 2, title: 'Featured', name: 'featured' },
-          ].map((item) => (
+          {LIST_CATEGORIES.categories.map((item) => (
             <Button
               type="button"
               key={item.id}
@@ -60,12 +58,7 @@ export default function CategoryBar({
         </Layout>
         <Layout>
           All Products
-          {[
-            { id: 1, title: 'Beds', name: 'beds' },
-            { id: 2, title: 'Sofas', name: 'sofas' },
-            { id: 3, title: 'Tables & desks', name: 'tables' },
-            { id: 4, title: 'Chairs', name: 'chairs' },
-          ].map((item) => (
+          {LIST_CATEGORIES.products.map((item) => (
             <Button
               type="button"
               key={item.id}
