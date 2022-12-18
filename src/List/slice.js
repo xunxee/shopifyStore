336f0ac01dfa78361c82e8b517b9510a8567ca0b
gunhee-jeong <kyle4619@naver.com>
2022-12-18 20:29:32 +0900

    Add: slice.js, checkUrl 함수의 액션을 테스트 및 구현 - 사용자가 클릭한 카테고리를 state에 저장하고 - checkUrl 함수의 중첩 함수인 makePathname 함수와 makeSearch 함수를 사용하여 url 만든다.

--- src/List/slice.js
+++ src/List/slice.js
@@ -1,11 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+import LIST_CATEGORIES from '../../fixtures/listCategoriesCollection';
+
 const { actions, reducer } = createSlice({
   name: 'list',
   initialState: {
-    lightOnCategory: '',
-    category: '',
     product: '',
+    category: '',
     sort: '',
     material: '',
   },
@@ -25,5 +26,62 @@ const { actions, reducer } = createSlice({
 export const {
   changeCategoriesDataField,
 } = actions;
+
+export function checkUrl({ name, belong }) {
+  return (dispatch, getState) => {
+    function returnEntries(kind) {
+      const { list } = getState();
+
+      const { url } = LIST_CATEGORIES;
+
+      return Object.entries(list)
+        .filter((category) => url[kind].includes(category[0]));
+    }
+
+    function makePathname() {
+      const pathname = ['/search'];
+
+      const pathnameEntries = returnEntries('pathnames');
+
+      for (let i = 0; i < pathnameEntries.length; i += 1) {
+        const category = pathnameEntries[i];
+
+        if (category[0] === 'category' && category[1]) {
+          pathname.push(`/${category[1]}`);
+        }
+
+        if (category[0] === 'product' && category[1]) {
+          pathname.push(`/product/${category[1]}`);
+        }
+      }
+
+      return pathname;
+    }
+
+    function makeSearch() {
+      const search = [];
+
+      const searchEntries = returnEntries('searchs');
+
+      for (let i = 0; i < searchEntries.length; i += 1) {
+        const category = searchEntries[i];
+
+        if (search.length >= 1 && category[1]) {
+          search.push(`&${category[0]}=${category[1]}`);
+        }
+
+        if (search.length === 0 && category[1]) {
+          search.push(`?${category[0]}=${category[1]}`);
+        }
+      }
+
+      return search;
+    }
+
+    dispatch(changeCategoriesDataField({ name, belong }));
+
+    return [...makePathname(), ...makeSearch()].join('');
+  };
+}
 
 export default reducer;
--- src/List/slice.test.js
+++ src/List/slice.test.js
@@ -1,6 +1,14 @@
+import thunk from 'redux-thunk';
+
+import configureStore from 'redux-mock-store';
+
 import reducer, {
   changeCategoriesDataField,
+  checkUrl,
 } from './slice';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
 
 describe('reducer', () => {
   describe('changeCategoriesDataField', () => {
@@ -18,6 +26,117 @@ describe('reducer', () => {
       );
 
       expect(category).toBe('new');
+    });
+  });
+});
+
+describe('actions', () => {
+  let store;
+
+  function makeMockStore({
+    product = '',
+    category = '',
+    sort = '',
+    material = '',
+  } = {}) {
+    return mockStore({
+      list: {
+        product,
+        category,
+        sort,
+        material,
+      },
+    });
+  }
+
+  describe('checkUrl', () => {
+    context('when it clicks New Arrivals', () => {
+      beforeEach(() => {
+        store = makeMockStore({
+          category: 'new',
+        });
+      });
+
+      it('changes url to /search/new', () => {
+        store.dispatch(checkUrl({
+          name: 'new',
+          belong: 'category',
+        }));
+
+        const actions = store.getActions();
+
+        expect(actions[0]).toEqual({
+          payload: { belong: 'category', name: 'new' },
+          type: 'list/changeCategoriesDataField',
+        });
+      });
+    });
+
+    context('when it clicks Sofas', () => {
+      beforeEach(() => {
+        store = makeMockStore({
+          product: 'sofas',
+        });
+      });
+
+      it('changes url to /search/product/sofas', () => {
+        store.dispatch(checkUrl({
+          name: 'sofas',
+          belong: 'product',
+        }));
+
+        const actions = store.getActions();
+
+        expect(actions[0]).toEqual({
+          payload: { belong: 'product', name: 'sofas' },
+          type: 'list/changeCategoriesDataField',
+        });
+      });
+    });
+
+    context('when it clicks Trending', () => {
+      beforeEach(() => {
+        store = makeMockStore({
+          sort: 'trending',
+        });
+      });
+
+      it('changes url to /search?sort=trending', () => {
+        store.dispatch(checkUrl({
+          name: 'trending',
+          belong: 'sort',
+        }));
+
+        const actions = store.getActions();
+
+        expect(actions[0]).toEqual({
+          payload: { belong: 'sort', name: 'trending' },
+          type: 'list/changeCategoriesDataField',
+        });
+      });
+    });
+
+    context('when there is sort and click fabric', () => {
+      beforeEach(() => {
+        store = makeMockStore({
+          sort: 'trending',
+          material: 'fabric',
+        });
+      });
+
+      it('changes url to /search?sort=trending&material=fabric', () => {
+        store.dispatch(checkUrl({
+          name: 'fabric',
+          belong: 'trending',
+        }));
+
+        const actions = store.getActions();
+
+        expect(actions[0]).toEqual({
+          payload: { belong: 'trending', name: 'fabric' },
+          type: 'list/changeCategoriesDataField',
+        });
+      });
     });
   });
 });
