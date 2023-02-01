5d9786ecdfa6f63f3d4115e1a2d6d2dc43e8221b
gunhee-jeong <kyle4619@naver.com>
2023-02-01 09:49:16 +0900

    Add: Header/slice.js의 changeSearchBarFields() 함수의 액션을 테스트 및 구현 - Header 컴포넌트의 Search Bar에서 사용자가 검색창을 입력 시, 해당 입력 data를 관리하기 위한 changeSearchBar 함수를 구현

---
+++ src/Header/slice.test.js
@@ -0,0 +1,40 @@
+import reducer, {
+  changeSearchBarFields,
+} from './slice';
+
+describe('reducer', () => {
+  context('when previous state is undefined', () => {
+    const initialState = {
+      searchBarFields: {
+        value: '',
+      },
+    };
+
+    it('returns initialState', () => {
+      const state = reducer(undefined, { type: 'action' });
+
+      expect(state).toEqual(initialState);
+    });
+  });
+
+  describe('changeSearchBarFields', () => {
+    context('when searchBar is changed', () => {
+      it('changes searchBar value', () => {
+        const initialState = {
+          searchBarFields: {
+            value: '',
+          },
+        };
+
+        const { searchBarFields: { value } } = reducer(
+          initialState,
+          changeSearchBarFields({
+            value: 'beds',
+          }),
+        );
+
+        expect(value).toBe('beds');
+      });
+    });
+  });
+});
