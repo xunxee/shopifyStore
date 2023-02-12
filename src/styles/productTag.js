ae274c7035bfba66c0fb5d511f0f6fd4d3b0e12a
gunhee-jeong <kyle4619@naver.com>
2023-02-12 11:48:47 +0900

    Add: List 컴포넌트와 ProductDetail 컴포넌트에서 사용할 CSS 스타일 파일을 추가

---
+++ src/styles/productTag.js
@@ -0,0 +1,22 @@
+import PALETTE from './palette';
+
+const { basicWhite } = PALETTE;
+
+const productTag = {
+  productName: {
+    padding: '1rem 1.5rem',
+    fontWeight: '700',
+    letterSpacing: '.4px',
+    backgroundColor: basicWhite,
+  },
+  priceName: {
+    padding: '.5rem 1.5rem 1rem',
+    fontSize: '14px',
+    lineHeight: '20px',
+    fontWeight: '600',
+    letterSpacing: '.35px',
+    backgroundColor: basicWhite,
+  },
+};
+
+export default productTag;
