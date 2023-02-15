3af8672ef01be9adae9d426d8c2e9ee479318b60
gunhee-jeong <kyle4619@naver.com>
2023-02-15 11:15:22 +0900

    Refactor: fixtures가 겹치는 data들을 하나로 합치는 리팩터링을 진행 - 기존 fixtures의 mockData들을 각 디렉터리에 해당하는 항목과 병합

--- fixtures/List/productDetail.js
+++
@@ -1,8 +0,0 @@
-const productDetail = {
-  id: 1,
-  title: 'Special Edition T-Shirt',
-  price: '$50.00 USD',
-  mainImage: 'https://user-images.githubusercontent.com/87808288/209490726-1626694c-35c8-49db-a082-0e6e1001310e.png',
-};
-
-export default productDetail;
---
+++ fixtures/ProductDetail/productDetail.js
@@ -0,0 +1,120 @@
+const productDetail = {
+  id: 1,
+  title: 'Special Edition T-Shirt',
+  price: '$50.00 USD',
+  imageList: [
+    'https://user-images.githubusercontent.com/87808288/210361413-0df984fc-ba95-44b4-a3f8-5ad25ca69fa9.png',
+    'https://user-images.githubusercontent.com/87808288/210361450-caec2f37-b159-417e-8e29-7841af4bb4f6.png',
+    'https://user-images.githubusercontent.com/87808288/210361485-b0962306-1e80-4390-ac12-55b891900240.png',
+    'https://user-images.githubusercontent.com/87808288/210361523-88a96ee5-b8c6-42a7-877d-135534e9afdc.png',
+    'https://user-images.githubusercontent.com/87808288/218358801-0ad605a6-9760-4f13-8c82-26b0abda077c.png',
+    'https://user-images.githubusercontent.com/87808288/218358841-a3b04df4-1dec-40bb-9ea6-217a1a6d7e1d.png',
+  ],
+  size: ['XS', 'S', 'M', 'L', 'XL'],
+  color: ['white', 'grey', 'darkturquoise', 'lightsalmon'],
+  details:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilisi morbi tempus. Praesent elementum facilisis leo vel fringilla. Congue mauris rhoncus aenean vel. Egestas sed tempus urna et pharetra pharetra massa massa ultricies.',
+  evaluation: {
+    starRating: 5,
+    review: [
+      "I'd love to.",
+      "It's neat",
+      'The quality is good',
+      "It's good to wear",
+    ],
+  },
+  banners: [
+    {
+      key: 1,
+      imgUrl:
+        'https://user-images.githubusercontent.com/87808288/210361413-0df984fc-ba95-44b4-a3f8-5ad25ca69fa9.png',
+    },
+    {
+      key: 2,
+      imgUrl:
+        'https://user-images.githubusercontent.com/87808288/210361450-caec2f37-b159-417e-8e29-7841af4bb4f6.png',
+    },
+    {
+      key: 3,
+      imgUrl:
+        'https://user-images.githubusercontent.com/87808288/210361485-b0962306-1e80-4390-ac12-55b891900240.png',
+    },
+    {
+      key: 4,
+      imgUrl:
+        'https://user-images.githubusercontent.com/87808288/210361523-88a96ee5-b8c6-42a7-877d-135534e9afdc.png',
+    },
+    {
+      key: 5,
+      imgUrl:
+        'https://user-images.githubusercontent.com/87808288/218358801-0ad605a6-9760-4f13-8c82-26b0abda077c.png',
+    },
+    {
+      key: 6,
+      imgUrl:
+        'https://user-images.githubusercontent.com/87808288/218358841-a3b04df4-1dec-40bb-9ea6-217a1a6d7e1d.png',
+    },
+    {
+      key: 7,
+      imgUrl:
+        'https://user-images.githubusercontent.com/87808288/210361413-0df984fc-ba95-44b4-a3f8-5ad25ca69fa9.png',
+    },
+    {
+      key: 8,
+      imgUrl:
+        'https://user-images.githubusercontent.com/87808288/210361450-caec2f37-b159-417e-8e29-7841af4bb4f6.png',
+    },
+    {
+      key: 9,
+      imgUrl:
+        'https://user-images.githubusercontent.com/87808288/210361485-b0962306-1e80-4390-ac12-55b891900240.png',
+    },
+    {
+      key: 10,
+      imgUrl:
+        'https://user-images.githubusercontent.com/87808288/210361523-88a96ee5-b8c6-42a7-877d-135534e9afdc.png',
+    },
+    {
+      key: 11,
+      imgUrl:
+        'https://user-images.githubusercontent.com/87808288/218358801-0ad605a6-9760-4f13-8c82-26b0abda077c.png',
+    },
+    {
+      key: 12,
+      imgUrl:
+        'https://user-images.githubusercontent.com/87808288/218358841-a3b04df4-1dec-40bb-9ea6-217a1a6d7e1d.png',
+    },
+    {
+      key: 13,
+      imgUrl:
+        'https://user-images.githubusercontent.com/87808288/210361413-0df984fc-ba95-44b4-a3f8-5ad25ca69fa9.png',
+    },
+    {
+      key: 14,
+      imgUrl:
+        'https://user-images.githubusercontent.com/87808288/210361450-caec2f37-b159-417e-8e29-7841af4bb4f6.png',
+    },
+    {
+      key: 15,
+      imgUrl:
+        'https://user-images.githubusercontent.com/87808288/210361485-b0962306-1e80-4390-ac12-55b891900240.png',
+    },
+    {
+      key: 16,
+      imgUrl:
+        'https://user-images.githubusercontent.com/87808288/210361523-88a96ee5-b8c6-42a7-877d-135534e9afdc.png',
+    },
+    {
+      key: 17,
+      imgUrl:
+        'https://user-images.githubusercontent.com/87808288/218358801-0ad605a6-9760-4f13-8c82-26b0abda077c.png',
+    },
+    {
+      key: 18,
+      imgUrl:
+        'https://user-images.githubusercontent.com/87808288/218358841-a3b04df4-1dec-40bb-9ea6-217a1a6d7e1d.png',
+    },
+  ],
+};
+
+export default productDetail;
