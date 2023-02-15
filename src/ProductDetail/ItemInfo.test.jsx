13ca4a5534eb099fac4082e3ce54e0968295e34d
gunhee-jeong <kyle4619@naver.com>
2023-02-15 14:32:02 +0900

    Add: 상품 정보를 화면에 렌더링하는 기본적인 액션을 테스트 및 구현

---
+++ src/ProductDetail/ItemInfo.test.jsx
@@ -0,0 +1,26 @@
+import { render } from '@testing-library/react';
+
+import ItemInfo from './ItemInfo';
+
+import PRODUCT_DETAIL from '../../fixtures/ProductDetail/productDetail';
+
+const { size, color, details, evaluation } = PRODUCT_DETAIL;
+
+describe('ItemInfo', () => {
+  function renderItemInfo() {
+    return render(
+      <ItemInfo
+        size={size}
+        color={color}
+        details={details}
+        evaluation={evaluation}
+      />,
+    );
+  }
+
+  it('renders product details', () => {
+    const { container } = renderItemInfo();
+
+    expect(container).toHaveTextContent('XL');
+  });
+});
