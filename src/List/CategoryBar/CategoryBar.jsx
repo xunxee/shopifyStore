5e703b1ba2037f85bb5545384ca7cf752d309e9f
gunhee-jeong <kyle4619@naver.com>
2023-02-17 14:51:40 +0900

    Refactor: emotion 객체의 이름을 리팩터링 - Container를 Wrapper로, Button을 StyledButton으로 리팩터링

--- src/List/CategoryBar/CategoryBar.jsx
+++ src/List/CategoryBar/CategoryBar.jsx
@@ -7,9 +7,9 @@ import LIST_CATEGORIES from '../../../fixtures/List/listCategoriesCollection';
 
 const { container, button } = LIST_CATEGORIES_STYLE;
 
-const Container = styled.div(container);
+const Wrapper = styled.div(container);
 
-const Button = styled.button(({ underLine, name }) => ({
+const StyledButton = styled.button(({ underLine, name }) => ({
   ...button,
   textDecoration: underLine === name ? 'underLine' : 'none',
 }));
@@ -25,12 +25,12 @@ export default function CategoryBar({ field, selectedItem, onClick }) {
   );
 
   return (
-    <Container>
+    <Wrapper>
       <div>
         <h1>{title}</h1>
         <ul>
           {data.map(({ value, subHeading }) => (
-            <Button
+            <StyledButton
               type="button"
               key={value}
               name={value}
@@ -38,10 +38,10 @@ export default function CategoryBar({ field, selectedItem, onClick }) {
               onClick={handleClick}
             >
               {subHeading}
-            </Button>
+            </StyledButton>
           ))}
         </ul>
       </div>
-    </Container>
+    </Wrapper>
   );
 }
