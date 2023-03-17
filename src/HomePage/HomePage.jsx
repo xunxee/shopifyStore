aaab6507d410d91c33b4e8ee7de6decf30d65823
gunhee-jeong <kyle4619@naver.com>
2023-03-17 18:51:00 +0900

    Modify: HomePage 컴포넌트를 화면의 끝까지 채우기 위한 속성을 추가

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,7 +1,9 @@
 import styled from '@emotion/styled';
 import HomePageContainer from './HomePageContainer';
 
-const Wrapper = styled.div({});
+const Wrapper = styled.div({
+  flex: 1,
+});
 
 export default function HomePage() {
   return (
