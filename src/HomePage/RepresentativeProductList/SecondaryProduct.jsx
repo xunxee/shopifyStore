a0a26cb8c2f08031b4db0ff706d8e0aa43e3eaae
gunhee-jeong <kyle4619@naver.com>
2023-03-17 18:54:25 +0900

    Add: SecondaryProduct 컴포넌트의 기본적인 CSS 스타일을 추가

--- src/HomePage/RepresentativeProductList/SecondaryProduct.jsx
+++ src/HomePage/RepresentativeProductList/SecondaryProduct.jsx
@@ -2,22 +2,54 @@ import styled from '@emotion/styled';
 
 import { setColorById } from '../../utils';
 
+import PRODUCT_TAG from '../../styles/productTag';
+
+const { productName, priceName } = PRODUCT_TAG;
+
 const Wrapper = styled.div({
-  width: '35%',
+  width: '30%',
+  cursor: 'pointer',
 });
 
 const StyledProductItem = styled.div(({ backgroundColor }) => (
   {
+    position: 'relative',
     backgroundColor,
+    width: '100%',
+    height: '50%',
   }
 ));
 
-const StyledProductTag = styled.div({});
+const StyledProductTag = styled.div({
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  zIndex: 0,
+  paddingRight: '4rem',
+  overflow: 'hidden',
+  '& h3': {
+    ...productName,
+    fontSize: '2rem',
+    lineHeight: '2rem',
+    boxDecorationBreak: 'clone',
+  },
+  '& span': {
+    ...priceName,
+    display: 'inline-block',
+  },
+});
 
 const StyledImgBox = styled.div({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '100%',
+  height: '100%',
+  overflow: 'hidden',
   '& img': {
-    maxWidth: '100%',
-    height: 'auto',
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover',
   },
 });
 
