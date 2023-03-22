8bd5f3077ca9484d879ac8ae91b1c3beb62aae2c
gunhee-jeong <kyle4619@naver.com>
2023-03-22 14:27:50 +0900

    Add: MarqueeWrapper 컴포넌트의 기본적인 CSS를 추가

--- src/HomePage/InfiniteMarquee/AutoSlide.jsx
+++ src/HomePage/InfiniteMarquee/AutoSlide.jsx
@@ -1,10 +1,17 @@
+import styled from '@emotion/styled';
+
 import MarqueeWrapper from './MarqueeWrapper';
 
 export default function AutoSlide({ productList }) {
+  const Wrapper = styled.div({
+    display: 'flex',
+    width: '100%',
+  });
+
   return (
-    <>
+    <Wrapper>
       <MarqueeWrapper productList={productList} />
       <MarqueeWrapper productList={productList} />
-    </>
+    </Wrapper>
   );
 }
--- src/HomePage/InfiniteMarquee/MarqueeWrapper.jsx
+++ src/HomePage/InfiniteMarquee/MarqueeWrapper.jsx
@@ -1,16 +1,67 @@
+import styled from '@emotion/styled';
+
+import PALETTE from '../../styles/palette';
+
+const { dark, basicWhite } = PALETTE;
+
 export default function MarqueeWrapper({ productList }) {
+  const Wrapper = styled.div({
+    display: 'flex',
+    minWidth: '100%',
+  });
+
+  const StyledProductLayout = styled.a({
+    position: 'relative',
+    display: 'inline-block',
+    width: '100%',
+    overflow: 'hidden',
+    '& img': {
+      display: 'block',
+      width: '320px',
+      height: '320px',
+      backgroundColor: dark,
+    },
+  });
+
+  const StyledProductTag = styled.div({
+    position: 'absolute',
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    zIndex: 20,
+    width: '100%',
+    height: '100%',
+    marginRight: '6rem',
+    '& span': {
+      display: 'inline-block',
+      padding: '.75rem',
+      fontSize: '1.25rem',
+      lineHeight: '1.75rem',
+      fontWeight: '700',
+      backgroundColor: basicWhite,
+      color: dark,
+    },
+  });
+
   return (
-    <div>
+    <Wrapper>
       {productList.map(({
         id, title, mainImage,
       }) => (
-        <a key={id} href={`product/${id}`}>
-          <div>
+        <StyledProductLayout
+          key={id}
+          href={`product/${id}`}
+        >
+          <StyledProductTag>
             <span>{title}</span>
-          </div>
+          </StyledProductTag>
           <img src={mainImage} alt={title} />
-        </a>
+        </StyledProductLayout>
       ))}
-    </div>
+    </Wrapper>
   );
 }
